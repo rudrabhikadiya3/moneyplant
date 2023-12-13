6c577f623c214df4fb537dc202d9fc4b2514dcbc
raininfotech170-rudra <rudra170@webmail.rainit.in>
2023-12-13 09:44:52 +0530

    change map

--- pages/index.js
+++ pages/index.js
@@ -435,7 +435,7 @@ export default function Home({ user, pkgData }) {
                     <h4>Call:</h4>
                     <p>+1 5589 55488 55s</p>
                   </div>
-                  <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12097.433213460943!2d-74.0062269!3d40.7101282!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xb89d1fe6bc499443!2sDowntown+Conference+Center!5e0!3m2!1smk!2sbg!4v1539943755621" frameBorder={0} style={{ border: 0, width: '100%', height: 290 }} allowFullScreen />
+                  <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d119066.54586578794!2d72.73989525503343!3d21.15918020387031!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be04e59411d1563%3A0xfe4558290938b042!2sSurat%2C%20Gujarat!5e0!3m2!1sen!2sin!4v1702440749846!5m2!1sen!2sin" frameBorder={0} style={{ border: 0, width: '100%', height: 290 }} allowFullScreen />
                 </div>
               </div>
               <div className="col-lg-7 mt-5 mt-lg-0 d-flex align-items-stretch">
